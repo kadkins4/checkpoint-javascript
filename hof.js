1f6fc8ff7a5f5af00cc5b3f6a8e8a02c57216b7d
Kendall <adkins.kendall90@gmail.com>
2017-07-10 09:57:11 -0400

    higher order

--- hof.js
+++ hof.js
@@ -1,18 +1,22 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations
 
 var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
+  {name: 'Jane', age: 32},
+  {name: 'Jessica', age: 34},
+  {name: 'Jasmine', age: 39}
 ]
 
 // Use the `map` array method to create a new array containing the ages of each
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
+var ages = function (person) {
+  return people.age
+}
 
-
-
+var peoplesAges = people.map(ages)
 
 // Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
+
+var olderAge = people.filter(ages => ages.age > 35)
